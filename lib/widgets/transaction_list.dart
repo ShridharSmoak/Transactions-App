a1b60ada99c26767d3029498c83e0f3733184f09
ShridharSmoak <ghostfeli34@gmail.com>
2020-08-31 16:08:52 +0530

    Added modal bottom sheet and moved files around again

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../models/transaction.dart';
+
 class TransactionList extends StatelessWidget {
-  final _userTransactionsList;
-  TransactionList(this._userTransactionsList);
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ..._userTransactionsList.map((tx) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
           return Card(
-            child: Row(children: [
-              Container(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.all(20),
-                  child: Text('\$${tx.amount}',
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
                       style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.purple))),
-              Column(children: [
-                Container(
-                    margin: EdgeInsets.all(5),
-                    child: Text('${tx.title}',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Container(
-                    margin: EdgeInsets.all(5),
-                    child: Text(DateFormat().format(tx.date),
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-              ])
-            ]),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
-        }).toList()
-      ],
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
